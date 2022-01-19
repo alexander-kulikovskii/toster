8e2673b313bc9a1ddefd91ade863ea1c8d69831c
Alexander Kulikovskiy <alexander.kulikovski@gmail.com>
2022-01-19 23:16:08 +0200

    add instagram as third party sample

---
+++ samples/src/test/java/fi/epicbot/toster/samples/SampleThirdPartyApp.kt
@@ -0,0 +1,32 @@
+package fi.epicbot.toster.samples
+
+import fi.epicbot.toster.TosterTest
+import fi.epicbot.toster.Config
+import fi.epicbot.toster.Screens
+import fi.epicbot.toster.model.SwipeMove
+
+class SampleThirdPartyApp : TosterTest(
+    Config {
+        applicationName("Instagram")
+        applicationPackageName("com.instagram.android")
+        doNotDeleteAndInstallApk()
+        report {
+            enable(true)
+        }
+        devices {
+            phone("UUID") // get from `adb devices`
+        }
+    },
+    Screens {
+        screen {
+            name("Main screen") // Assumes that you are already logged in
+            url("com.instagram.android.activity.MainTabActivity")
+            actions {
+                repeat(10) {
+                    swipe(SwipeMove.BottomToTop, delayMillis = 500)
+                    takeMemoryAllocation()
+                }
+            }
+        }
+    }
+)
