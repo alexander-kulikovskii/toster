bea3d81cdb303766e8dc684b3c1efcfec44a4d9d
Alexander Kulikovskiy <alexander.kulikovski@gmail.com>
2022-03-30 18:35:12 +0300

    add dexcount, add dep checker, update dependencies

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
     id("kotlinx-serialization")
 }
+apply {
+    from("${rootDir}/gradle/dependency-updates.gradle")
+}
 
 dependencies {
     implementation ("androidx.core:core-ktx:1.7.0")
