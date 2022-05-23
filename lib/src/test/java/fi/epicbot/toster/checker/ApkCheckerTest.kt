924c0fcd6b3276e29b2d066df13e2aac7cf4fe1e
Alexander Kulikovskiy <alexander.kulikovski@gmail.com>
2022-05-23 20:58:04 +0300

    update tests for checkers (#145)
    * update tests for checkers
    
    * fix detekt

---
+++ lib/src/test/java/fi/epicbot/toster/checker/ApkCheckerTest.kt
@@ -0,0 +1,103 @@
+package fi.epicbot.toster.checker
+
+import fi.epicbot.toster.WhenWithException
+import fi.epicbot.toster.WhenWithoutException
+import fi.epicbot.toster.model.Apk
+import io.kotest.core.spec.style.BehaviorSpec
+
+class ApkCheckerPositiveTest : BehaviorSpec({
+    correctData.forEach() { inputConfig ->
+        Given("ApkChecker with $inputConfig") {
+            val configChecker = ApkChecker(inputConfig)
+            WhenWithoutException("Check config without any exception") {
+                configChecker.check()
+            }
+        }
+    }
+})
+
+class ApkCheckerNegativeTest : BehaviorSpec({
+    exceptionData.forEach() { (inputConfig, expectedMessage) ->
+        Given("ApkChecker with $inputConfig") {
+            val configChecker = ApkChecker(inputConfig)
+            WhenWithException<IllegalArgumentException>(
+                "Check config with exception",
+                expectedMessage
+            ) {
+                configChecker.check()
+            }
+        }
+    }
+})
+
+private const val NOT_EMPTY_FIELD = "Test"
+private const val EMPTY_APK_LIST = "Apk list shouldn't be empty"
+private const val EMPTY_APK_URL = "Apk url shouldn't be empty"
+private const val EMPTY_APK_PREFIX = "Apk prefix shouldn't be empty"
+private const val NOT_UNIQUE_PREFIXES = "All prefixes should be unique: <Test> - 2 times"
+private const val BLANK_SHELL_BEFORE_SCREEN = "Set non empty shell before"
+
+private val correctData = listOf(
+    listOf(
+        Apk(
+            url = NOT_EMPTY_FIELD,
+        )
+    ),
+    listOf(
+        Apk(
+            url = NOT_EMPTY_FIELD,
+            shellsBefore = arrayOf(NOT_EMPTY_FIELD),
+        )
+    ),
+    listOf(
+        Apk(
+            url = NOT_EMPTY_FIELD,
+            shellsBefore = arrayOf(NOT_EMPTY_FIELD),
+            prefix = NOT_EMPTY_FIELD,
+        )
+    ),
+    listOf(
+        Apk(
+            url = NOT_EMPTY_FIELD,
+            shellsBefore = arrayOf(NOT_EMPTY_FIELD),
+            prefix = NOT_EMPTY_FIELD,
+        ),
+        Apk(
+            url = NOT_EMPTY_FIELD,
+            shellsBefore = arrayOf(NOT_EMPTY_FIELD),
+            prefix = NOT_EMPTY_FIELD + "2",
+        )
+    ),
+)
+
+private val exceptionData = mapOf(
+    listOf<Apk>() to EMPTY_APK_LIST,
+    listOf(
+        Apk()
+    ) to EMPTY_APK_URL,
+    listOf(
+        Apk(
+            url = NOT_EMPTY_FIELD,
+            prefix = "",
+        ),
+    ) to EMPTY_APK_PREFIX,
+    listOf(
+        Apk(
+            url = NOT_EMPTY_FIELD,
+            prefix = NOT_EMPTY_FIELD,
+            shellsBefore = arrayOf(" ")
+        ),
+    ) to BLANK_SHELL_BEFORE_SCREEN,
+    listOf(
+        Apk(
+            url = NOT_EMPTY_FIELD,
+            shellsBefore = arrayOf(NOT_EMPTY_FIELD),
+            prefix = NOT_EMPTY_FIELD,
+        ),
+        Apk(
+            url = NOT_EMPTY_FIELD,
+            shellsBefore = arrayOf(NOT_EMPTY_FIELD),
+            prefix = NOT_EMPTY_FIELD,
+        )
+    ) to NOT_UNIQUE_PREFIXES
+)
