1ba58a1bfbbbdaaf0d10fb7dcd1697609a4b99f7
Alexander Kulikovskiy <alexander.kulikovski@gmail.com>
2022-05-29 22:28:03 +0300

    add tests for Before and After screens blocks (#159)
    * add tests for Before and After screens blocks
    
    * fix detekt

---
+++ lib/src/test/java/fi/epicbot/toster/extension/actionexecutor/RunAfterScreensTest.kt
@@ -0,0 +1,106 @@
+package fi.epicbot.toster.extension.actionexecutor
+
+import fi.epicbot.toster.CoVerify
+import fi.epicbot.toster.executor.ActionExecutor
+import fi.epicbot.toster.extension.runAfterScreens
+import fi.epicbot.toster.model.Action
+import fi.epicbot.toster.model.Config
+import fi.epicbot.toster.model.Density
+import fi.epicbot.toster.model.ScreenSize
+import fi.epicbot.toster.report.model.Common
+import fi.epicbot.toster.report.model.ReportScreen
+import io.kotest.core.config.ProjectConfiguration
+import io.kotest.core.spec.SpecRef
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.engine.TestEngineLauncher
+import io.kotest.engine.listener.CollectingTestEngineListener
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlin.reflect.KClass
+
+private open class RunAfterScreensTestSpec(
+    actionExecutor: ActionExecutor,
+    config: Config,
+    reportScreen: MutableList<ReportScreen>,
+) : DescribeSpec({
+    describe("test") {
+        reportScreen.add(
+            actionExecutor.runAfterScreens(config)
+        )
+    }
+})
+
+internal class RunAfterScreensTest : BehaviorSpec({
+
+    val collector = CollectingTestEngineListener()
+
+    applySizeTestData.forEach { runBeforeScreensModel ->
+        Given("run Shells For Apk ${runBeforeScreensModel.title}") {
+            val projectConfiguration = ProjectConfiguration()
+            val tmpList = mutableListOf<ReportScreen>()
+
+            val actionExecutor = mockk<ActionExecutor>(relaxed = true)
+            coEvery { actionExecutor.execute(any()) } returns Common(2, "", 3, 4)
+
+            When("execute action") {
+                val ref = SpecRef.Function({
+                    RunAfterScreensTestSpec(
+                        actionExecutor,
+                        runBeforeScreensModel.config,
+                        tmpList,
+                    )
+                }, RunAfterScreensTestSpec::class)
+                TestEngineLauncher(collector, projectConfiguration, emptyList(), listOf(ref), null)
+                    .withExtensions()
+                    .launch()
+                CoVerify("finishEnvironment called") {
+                    actionExecutor.finishEnvironment()
+                }
+                runBeforeScreensModel.expectedActions.forEach {
+                    CoVerify("${it.simpleName} called") {
+                        actionExecutor.execute(ofType(it))
+                    }
+                }
+                Then("called test") {
+                    collector.result("test")?.isSuccess shouldBe true
+                }
+                Then("report name should be After") {
+                    tmpList[0].name shouldBe "After"
+                }
+            }
+        }
+    }
+})
+
+private class RunAfterScreensTestModel(
+    val title: String,
+    val config: Config,
+    val expectedActions: List<KClass<out Action>>,
+)
+
+private val applySizeTestData = listOf(
+    RunAfterScreensTestModel(
+        "Default config",
+        Config(),
+        listOf(
+            Action.HideDemoMode::class,
+        ),
+    ),
+    RunAfterScreensTestModel(
+        "Custom config",
+        Config().apply {
+            shellsAfterAllScreens = arrayOf("shell 1", "shell 2")
+            globalScreenDensity = Density.XHDPI
+            globalScreenSize = ScreenSize(2, 3)
+            useDemoMode = false
+        },
+        listOf(
+            Action.ShellAfterAllScreens::class,
+            Action.ShellAfterAllScreens::class,
+            Action.ResetScreenDensity::class,
+            Action.ResetScreenSize::class,
+        ),
+    ),
+)
---
+++ lib/src/test/java/fi/epicbot/toster/extension/actionexecutor/RunBeforeScreensTest.kt
@@ -0,0 +1,126 @@
+package fi.epicbot.toster.extension.actionexecutor
+
+import fi.epicbot.toster.CoVerify
+import fi.epicbot.toster.executor.ActionExecutor
+import fi.epicbot.toster.extension.runBeforeScreens
+import fi.epicbot.toster.model.Action
+import fi.epicbot.toster.model.BufferDimension
+import fi.epicbot.toster.model.BufferSize
+import fi.epicbot.toster.model.Config
+import fi.epicbot.toster.model.Density
+import fi.epicbot.toster.model.ScreenSize
+import fi.epicbot.toster.report.model.Common
+import fi.epicbot.toster.report.model.ReportScreen
+import io.kotest.core.config.ProjectConfiguration
+import io.kotest.core.spec.SpecRef
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.engine.TestEngineLauncher
+import io.kotest.engine.listener.CollectingTestEngineListener
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlin.reflect.KClass
+
+private open class RunBeforeScreensTestSpec(
+    actionExecutor: ActionExecutor,
+    config: Config,
+    apkUrl: String,
+    reportScreen: MutableList<ReportScreen>,
+) : DescribeSpec({
+    describe("test") {
+        reportScreen.add(
+            actionExecutor.runBeforeScreens(config, apkUrl)
+        )
+    }
+})
+
+internal class RunBeforeScreensTest : BehaviorSpec({
+
+    val collector = CollectingTestEngineListener()
+
+    applySizeTestData.forEach { runBeforeScreensModel ->
+        Given("run Shells For Apk ${runBeforeScreensModel.title}") {
+            val projectConfiguration = ProjectConfiguration()
+            val tmpList = mutableListOf<ReportScreen>()
+
+            val actionExecutor = mockk<ActionExecutor>(relaxed = true)
+            coEvery { actionExecutor.execute(any()) } returns Common(2, "", 3, 4)
+
+            When("execute action") {
+                val ref = SpecRef.Function({
+                    RunBeforeScreensTestSpec(
+                        actionExecutor,
+                        runBeforeScreensModel.config,
+                        runBeforeScreensModel.apkUrl,
+                        tmpList,
+                    )
+                }, RunBeforeScreensTestSpec::class)
+                TestEngineLauncher(collector, projectConfiguration, emptyList(), listOf(ref), null)
+                    .withExtensions()
+                    .launch()
+                CoVerify("prepareEnvironment called") {
+                    actionExecutor.prepareEnvironment()
+                }
+                runBeforeScreensModel.expectedActions.forEach {
+                    CoVerify("${it.simpleName} called") {
+                        actionExecutor.execute(ofType(it))
+                    }
+                }
+                Then("called test") {
+                    collector.result("test")?.isSuccess shouldBe true
+                }
+                Then("report name should be Before") {
+                    tmpList[0].name shouldBe "Before"
+                }
+            }
+        }
+    }
+})
+
+private class RunBeforeScreensTestModel(
+    val title: String,
+    val config: Config,
+    val apkUrl: String,
+    val expectedActions: List<KClass<out Action>>,
+)
+
+private val applySizeTestData = listOf(
+    RunBeforeScreensTestModel(
+        "Default config",
+        Config(),
+        "url default",
+        listOf(
+            Action.ClearAppData::class,
+            Action.DeleteApk::class,
+            Action.InstallApk::class,
+            Action.HideDemoMode::class,
+            Action.SetDemoModeEnable::class,
+            Action.ShowDemoMode::class,
+            Action.HideGpuOverdraw::class,
+        ),
+    ),
+    RunBeforeScreensTestModel(
+        "Custom config",
+        Config().apply {
+            restartAdbServiceBeforeEachDevice = true
+            globalLogcatBufferSize = BufferSize(2, BufferDimension.MEGABYTES)
+            shellsBeforeAllScreens = arrayOf("shell 1", "shell 2")
+            globalScreenDensity = Density.XHDPI
+            globalScreenSize = ScreenSize(2, 3)
+            deleteAndInstallApk = false
+            useDemoMode = false
+        },
+        "url custom",
+        listOf(
+            Action.RestartAdbService::class,
+            Action.SetLogcatBufferSize::class,
+            Action.ShellBeforeAllScreens::class,
+            Action.ShellBeforeAllScreens::class,
+            Action.SetScreenDensity::class,
+            Action.SetScreenSize::class,
+            Action.HideDemoMode::class,
+            Action.HideGpuOverdraw::class,
+        ),
+    ),
+)
