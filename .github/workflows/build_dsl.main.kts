4ef55a5019282d26f3d85cf5a8e3cb8fc864cb63
Alexander Kulikovskiy <alexander.kulikovski@gmail.com>
2022-08-20 14:57:26 +0300

    add new workflow

--- .github/workflows/build_dsl.main.kts
+++ .github/workflows/build_dsl.main.kts
@@ -3,23 +3,11 @@
 @file:Import("Common.main.kts")
 
 import it.krzeminski.githubactions.domain.RunnerType.UbuntuLatest
-import it.krzeminski.githubactions.domain.RunnerType.MacOSLatest
-import it.krzeminski.githubactions.actions.reactivecircus.AndroidEmulatorRunnerV2
-import it.krzeminski.githubactions.actions.reactivecircus.AndroidEmulatorRunnerV2.Arch.Arm64V8a
 import it.krzeminski.githubactions.domain.triggers.PullRequest
 import it.krzeminski.githubactions.dsl.workflow
 import it.krzeminski.githubactions.yaml.writeToFile
 import it.krzeminski.githubactions.dsl.expressions.expr
 import java.nio.file.Paths
-
-private val testNameList = listOf(
-    "SampleDensityTest",
-    "SampleFontSizeTest",
-    "SampleLanguageTest",
-    "SampleOverdrawTest",
-    "SampleParamsTest",
-    "SampleMultiApkTest"
-)
 
 workflow(
     name = "Build DSL",
@@ -61,40 +49,4 @@ workflow(
         prepareEnvironment()
         runGradleTask("Run Debug Unit Tests", lib.gradle("assembleRelease"))
     }
-
-    job(
-        id = "test-sample",
-        name = "Test samples",
-        runsOn = MacOSLatest,
-        needs = listOf(staticAnalysisJob),
-        _customArguments = mapOf(
-            "strategy" to mapOf(
-                "fail-fast" to false,
-                "matrix" to mapOf(
-                    "api-level" to listOf(26, 31),
-                )
-            )
-        ),
-    ) {
-        prepareEnvironment()
-        uses(
-            name = "Run tests for \${{ matrix.api-level }}",
-            action = AndroidEmulatorRunnerV2(
-                _customInputs = mapOf(
-                    "api-level" to "\${{ matrix.api-level }}"
-                ),
-                apiLevel = 0,
-                arch = Arm64V8a,
-                disableAnimations = true,
-                emulatorOptions = "-no-snapshot-save -no-window -gpu swiftshader_indirect -noaudio -no-boot-anim -camera-back none",
-                script = "adb devices\n" + testNameList.map { name -> "./gradlew :samples:testDebug --tests \"fi.epicbot.toster.samples.${name}\" --stacktrace" }
-                    .joinToString("\n")
-            )
-        )
-        publishResults(
-            artifactName = "SampleTestOutput_api_\${{ matrix.api-level }}",
-            "lib/build/test-results/**/*.xml",
-        )
-    }
-
 }.writeToFile()
