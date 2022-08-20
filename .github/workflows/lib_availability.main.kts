7d3018d2564ba1ac3612929f313a71248cbd89d7
Alexander Kulikovskiy <alexander.kulikovski@gmail.com>
2022-08-20 13:36:21 +0300

    return old combination for lib checker

--- .github/workflows/lib_availability.main.kts
+++ .github/workflows/lib_availability.main.kts
@@ -11,6 +11,8 @@ import it.krzeminski.githubactions.yaml.writeToFile
 import it.krzeminski.githubactions.dsl.expressions.expr
 import java.nio.file.Paths
 
+private val LAST_LIB_VERSION = "0.3.1"
+
 workflow(
     name = "Library availability",
     on = listOf(
@@ -25,11 +27,37 @@ workflow(
         id = "lib_availability",
         name = "lib-availability",
         runsOn = UbuntuLatest,
-        strategyMatrix = mapOf(
-            "lib-versions" to listOf("0.3.1", "0.3.0", "0.2.9", "0.2.8", "0.2.7"),
-            "gradle-plugins" to listOf("7.5.1", "7.4"),
-            "gradle-tools" to listOf("7.1.1"),
-            "kotlin-version" to listOf("1.7.10", "1.6.21", "1.5.31"),
+        _customArguments = mapOf(
+            "strategy" to mapOf(
+                "fail-fast" to false,
+                "matrix" to mapOf(
+                    "lib-versions" to listOf(LAST_LIB_VERSION, "0.3.0", "0.2.9", "0.2.8", "0.2.7"),
+                    "gradle-plugins" to listOf("7.5.1"),
+                    "gradle-tools" to listOf("7.1.3"),
+                    "kotlin-version" to listOf("1.7.10"),
+
+                    "include" to listOf(
+                        mapOf(
+                            "lib-versions" to LAST_LIB_VERSION,
+                            "gradle-plugins" to "7.0.2",
+                            "gradle-tools" to "7.0.2",
+                            "kotlin-version" to "1.5.31",
+                        ),
+                        mapOf(
+                            "lib-versions" to LAST_LIB_VERSION,
+                            "gradle-plugins" to "6.2.2",
+                            "gradle-tools" to "3.5.3",
+                            "kotlin-version" to "1.3.71",
+                        ),
+                        mapOf(
+                            "lib-versions" to LAST_LIB_VERSION,
+                            "gradle-plugins" to "6.1.1",
+                            "gradle-tools" to "4.0.0",
+                            "kotlin-version" to "1.3.72",
+                        ),
+                    )
+                )
+            )
         ),
     ) {
         prepareEnvironment()
